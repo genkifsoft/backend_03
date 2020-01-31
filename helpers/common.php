5c75a22487b98300b7690cde0ba12c620b99c047
DangQuy <genkifsoft@gmail.com>
2020-01-31 08:52:08 +0700

    comit source code

---
+++ helpers/common.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Common global system helper functions
+|--------------------------------------------------------------------------
+|
+| This file contains system helper functions that might be used in this
+| application. Some helpers could be short-handed by easy-to-remember
+| functions instead of calling long class names and methods.
+|
+ */
+if (!function_exists('utf8_url')) {
+    function utf8_url($str) {
+        $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", "a", $str);
+        $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", "e", $str);
+        $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", "i", $str);
+        $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", "o", $str);
+        $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", "u", $str);
+        $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", "y", $str);
+        $str = preg_replace("/(đ)/", "d", $str);
+        $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", "A", $str);
+        $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", "E", $str);
+        $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", "I", $str);
+        $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", "O", $str);
+        $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", "U", $str);
+        $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", "Y", $str);
+        $str = preg_replace("/(Đ)/", "D", $str);
+        //$str = str_replace(" ", "-", str_replace("&*#39;","",$str));
+        return $str;
+    }
+}
+
+
+if (!function_exists('createSubId')) {
+    function createSubId($length = 30) {
+        $characters = '0123456789';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+}
+
+if (!function_exists('createID'))
+{
+    function createID($count)
+    {
+        $string = createSubId(3);
+        $createID  = $string. str_pad(++$count, 2, '0', STR_PAD_LEFT)
+                .substr(strtotime(time_now()), -3, 3);
+
+        return $createID;
+    }
+}
+
+if (!function_exists('time_now')) {
+    function time_now() {
+        return (new \DateTime())->format('Y-m-d H:i:s');
+    }
+}
+
+if (!function_exists('convert_pagination')) {
+    function convert_pagination($string) {
+        return str_replace(' ', '-', strtolower(utf8_url($string)));
+    }
+}
---
+++ helpers/define.php
@@ -0,0 +1,3 @@
+<?php
+
+define("STR_WORD_COUNT", 300);
